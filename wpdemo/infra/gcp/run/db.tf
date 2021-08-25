56769fb3efc1557fd94f09901efab73b00cba78b
pranaysinghdev <pranaysinghdev@gmail.com>
2021-08-25 20:27:35 +0530

    added variables

--- wpdemo/infra/gcp/run/db.tf
+++ wpdemo/infra/gcp/run/db.tf
@@ -1,20 +1,20 @@
 
 resource "google_sql_database" "main" {
   name     = "main"
-  instance = google_sql_database_instance.db_primary.name
+  instance = google_sql_database_instance.db_instance.name
 }
 
-resource "google_sql_database_instance" "db_primary" {
+resource "google_sql_database_instance" "db_instance" {
   name             = "db-primary"
   database_version = "MYSQL_5_7"
   deletion_protection = false
   settings {
-    tier              = var.instance_type
+    tier              = var.db_instance_type
   }
 }
 
 resource "google_sql_user" "db_user" {
-  name     = var.user
-  instance = google_sql_database_instance.db_primary.name
-  password = var.password
+  name     = var.db_user
+  instance = google_sql_database_instance.db_instance.name
+  password = var.db_password
 }
