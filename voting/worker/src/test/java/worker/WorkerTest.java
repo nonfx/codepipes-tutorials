3104c8ab0a02a15225f015f34a7f9a25c57d0e0d
ajain-cldcvr <70101077+ajain-cldcvr@users.noreply.github.com>
2022-03-02 16:14:01 +0800

    VAN-2427 Add sample unit test case for java app (#31)

---
+++ voting/worker/src/test/java/worker/WorkerTest.java
@@ -0,0 +1,12 @@
+package worker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class WorkerTest {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
