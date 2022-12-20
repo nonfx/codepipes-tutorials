57e438b99d7e78b7a5564fcd42c99766e653f625
Doug Shelley <doug@cldcvr.com>
2022-12-20 15:41:56 -0500

    VAN-3322 Upgrade default TF version to 1.3.6
    The "map" function has now been removed and must be changed to "tomap"

--- wpdemo/infra/aws/eks/vpc.tf
+++ wpdemo/infra/aws/eks/vpc.tf
@@ -7,36 +7,36 @@
 #
 
 resource "aws_vpc" "demo" {
-  cidr_block = var.cluster_ipv4_cidr
-  enable_dns_hostnames   = true
-  tags = map(
-    "Name", "${var.cluster-name}-${random_string.cluster.id}",
-    "kubernetes.io/cluster/${var.cluster-name}-${random_string.cluster.id}", "shared",
-  )
+  cidr_block           = var.cluster_ipv4_cidr
+  enable_dns_hostnames = true
+  tags = tomap({
+    "Name"                                                                  = "${var.cluster-name}-${random_string.cluster.id}"
+    "kubernetes.io/cluster/${var.cluster-name}-${random_string.cluster.id}" = "shared"
+  })
 }
 
 resource "aws_subnet" "demo" {
   depends_on = [aws_vpc.demo]
-  count = 2
+  count      = 2
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.demo.id
 
-  tags = map(
-    "Name", "${var.cluster-name}-${random_string.cluster.id}",
-    "kubernetes.io/cluster/${var.cluster-name}-${random_string.cluster.id}", "shared",
-    "kubernetes.io/role/elb","1",
-  )
+  tags = tomap({
+    "Name"                                                                  = "${var.cluster-name}-${random_string.cluster.id}"
+    "kubernetes.io/cluster/${var.cluster-name}-${random_string.cluster.id}" = "shared"
+    "kubernetes.io/role/elb"                                                = "1"
+  })
 }
 
 resource "aws_subnet" "dbsubnet" {
-  depends_on=[aws_vpc.demo]
-  count = 2
-  vpc_id     = aws_vpc.demo.id
-  cidr_block = "10.0.${count.index + 2}.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  depends_on        = [aws_vpc.demo]
+  count             = 2
+  vpc_id            = aws_vpc.demo.id
+  cidr_block        = "10.0.${count.index + 2}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "Postgres"
   }
@@ -44,7 +44,7 @@ resource "aws_subnet" "dbsubnet" {
 
 resource "aws_internet_gateway" "demo" {
   depends_on = [aws_vpc.demo]
-  vpc_id = aws_vpc.demo.id
+  vpc_id     = aws_vpc.demo.id
 
   tags = {
     Name = var.vpc-eks-tag-name
@@ -53,7 +53,7 @@ resource "aws_internet_gateway" "demo" {
 
 resource "aws_route_table" "demo" {
   depends_on = [aws_internet_gateway.demo]
-  vpc_id = aws_vpc.demo.id
+  vpc_id     = aws_vpc.demo.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -63,7 +63,7 @@ resource "aws_route_table" "demo" {
 
 resource "aws_route_table_association" "demo" {
   depends_on = [aws_route_table.demo]
-  count = 2
+  count      = 2
 
   subnet_id      = aws_subnet.demo.*.id[count.index]
   route_table_id = aws_route_table.demo.id
