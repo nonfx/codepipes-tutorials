59ac97a84f976d94d998ee50930942c8daf06069
rajatsharma111 <rajat.sharma@cldcvr.com>
2023-01-31 11:00:42 +0530

    VAN-3633 Added random string in vpc connector

--- dependency-demo/borat-gcs-redis-automation-test/infra/variable.tf
+++ dependency-demo/borat-gcs-redis-automation-test/infra/variable.tf
@@ -14,3 +14,11 @@ variable "IP_CIDR_RANGE" {
   type        = string
   default     = "10.0.0.0/28"
 }
+
+provider "random" {}
+
+resource "random_string" "random" {
+  length    = 5
+  special   = false
+  min_lower = 5
+}
