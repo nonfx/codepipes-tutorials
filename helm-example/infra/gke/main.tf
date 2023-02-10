cc4951e61ef59aed9e5c3160045c9e8262423e95
rajatsharma111 <rajat.sharma@cldcvr.com>
2023-02-10 10:47:23 +0530

    VAN-3711 reduced node count

--- helm-example/infra/gke/main.tf
+++ helm-example/infra/gke/main.tf
@@ -8,7 +8,8 @@ resource "google_container_cluster" "primary" {
   project            = var.GOOGLE_PROJECT
   name               = format("%s-%s", var.CLUSTER_NAME, random_string.random.result)
   location           = var.location
-  initial_node_count = 3
+  initial_node_count = 1
+  remove_default_node_pool = true
   node_config {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
