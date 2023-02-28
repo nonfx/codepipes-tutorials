4efb78aafcb568a904173fdca7649323f9939b01
Pranay Singh <pranaysinghdev@gmail.com>
2023-02-28 14:12:27 +0530

    updated iam policies

---
+++ tfs/aws-ecr-apprunner/output.tf
@@ -0,0 +1,8 @@
+
+output "aws_ecr_role" {
+  value = aws_iam_role.ecr_role.arn
+}
+
+output "aws_instance_role" {
+  value = aws_iam_role.instance_role.arn
+}
--- tfs/aws-elasticcache/output.tf
+++
@@ -1,6 +0,0 @@
-output "address" {
-  value = aws_elasticache_cluster.redis_version_non_compliant_3.cache_nodes[0].address
-}
-output "port" {
-  value = aws_elasticache_cluster.redis_version_non_compliant_3.cache_nodes[0].port
-}
