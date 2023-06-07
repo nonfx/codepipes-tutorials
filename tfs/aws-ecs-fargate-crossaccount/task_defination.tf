af31cee8109bc8cda1a1ce2da174452d375c40f2
Pranay Singh <pranay@cldcvr.com>
2023-06-07 17:49:34 +0530

    VAN-4162 Added a sample tf for cross account access in AWS

--- tfs/aws-ecs-fargate-crossaccount/task_defination.tf
+++ tfs/aws-ecs-fargate-crossaccount/task_defination.tf
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "nginx_task" {
   [
     {
       "name": "nginx",
-      "image": "nginx:latest",
+      "image": "569700770868.dkr.ecr.us-east-1.amazonaws.com/ngnix",
       "cpu": 256,
       "memory": 512,
       "portMappings": [
