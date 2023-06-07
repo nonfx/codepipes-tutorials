0de07d3512385ae1e705b6f455c9f16823787b2b
Pranay Singh <pranay@cldcvr.com>
2023-06-07 12:17:13 +0530

    VAN-4162 Added a sample tf for cross account access in AWS

--- tfs/aws-ecs-fargate-crossaccount/task_defination.tf
+++ tfs/aws-ecs-fargate-crossaccount/task_defination.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "nginx_task" {
   family = "nginx-task"
-  # execution_role_arn       = aws_iam_role.task_execution_role.arn
-  # task_role_arn            = aws_iam_role.task_role.arn
+  execution_role_arn       = aws_iam_role.ecs-iam-role.arn
+  task_role_arn            = aws_iam_role.ecs-iam-role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
 
@@ -105,38 +105,27 @@ resource "aws_lb_listener" "nginx_listener" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = data.aws_vpc.existing_vpc.id
 }
-# resource "aws_iam_role" "task_execution_role" {
-#   name               = "ecsTaskExecutionRole"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "ecs-tasks.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
 
-# resource "aws_iam_role" "task_role" {
-#   name               = "ecsTaskRole"
-#   assume_role_policy = <<EOF
-#   {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#         "Effect": "Allow",
-#         "Principal": {
-#           "Service": "ecs-tasks.amazonaws.com"
-#         },
-#         "Action": "sts:AssumeRole"
-#       }
-#     ]
-#   }
-#   EOF
-# }
+
+resource "aws_iam_role" "ecs-iam-role" {
+  name = "ecs-iam-role-v2"
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/SecretsManagerReadWrite", "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy", "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess","arn:aws:iam::aws:policy/CloudWatchFullAccess"]
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "",
+          "Effect": "Allow",
+          "Principal": {
+              "Service": "ecs-tasks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+      }
+  ]
+}
+EOF
+
+}
