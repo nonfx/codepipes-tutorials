c906001e45e159a5ed38fbdd55f1fab0400cdfc1
cc-shekher <chandrashekhar@cldcvr.com>
2023-07-04 15:31:14 +0530

    VAN-2721 env provider for dependency

---
+++ dependency-demo/env-provider/infra/aws/output.tf
@@ -0,0 +1,14 @@
+output "bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
+}
+output "aws_ecr_role" {
+  value = aws_iam_role.ecr_role.arn
+}
+
+output "aws_instance_role" {
+  value = aws_iam_role.instance_role.arn
+}
+
+output "aws_region" {
+  value = var.aws_region
+}
